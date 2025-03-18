5bf441a40ced79175be6400833b0d664a752039e
raghavgarg <raghav.garg@convegenius.ai>
2025-03-18 14:09:28 +0530

    refactor: forked video_player with renamed package

---
+++ video_player_platform_interface/test/video_player_web_options_test.dart
@@ -0,0 +1,32 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter_cached_video_player_platform_interface/video_player_platform_interface.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test(
+    'VideoPlayerOptions controls defaults to VideoPlayerWebOptionsControls.disabled()',
+    () {
+      const VideoPlayerWebOptions options = VideoPlayerWebOptions();
+      expect(options.controls, const VideoPlayerWebOptionsControls.disabled());
+    },
+  );
+
+  test(
+    'VideoPlayerOptions allowContextMenu defaults to true',
+    () {
+      const VideoPlayerWebOptions options = VideoPlayerWebOptions();
+      expect(options.allowContextMenu, isTrue);
+    },
+  );
+
+  test(
+    'VideoPlayerOptions allowRemotePlayback defaults to true',
+    () {
+      const VideoPlayerWebOptions options = VideoPlayerWebOptions();
+      expect(options.allowRemotePlayback, isTrue);
+    },
+  );
+}
